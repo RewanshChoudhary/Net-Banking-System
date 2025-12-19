174b1632908ba885e95ca32ebbd2bb08644db1c9
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2025-12-19 19:52:42 +0530

    v1

---
+++ src/main/java/com/example/netbanking/NetbankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.netbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbankingApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(NetbankingApplication.class, args);
+	}
+
+}
